d5cc2d8017dc37a9c9e98cc632bdec60805ce333
Md-Asikur <asikurrahaman997@gamil.com>
2023-01-06 04:27:44 -0800

    ll:

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,7 @@ import Loading from "./com_1/components/Loading";
 function App() {
   return (
     <>
-      <div style={{ margin: "0px 1px" }}>
+      <div style={{ margin: "0px 2px" }}>
         <BrowserRouter>
           <Header />
           <Loading />
